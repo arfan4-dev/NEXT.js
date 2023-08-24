c49c1fbddfea86bf3b7ab873972d37a831ddc3e7
MUHAMMAD ARFAN <106101890+fanijan786@users.noreply.github.com>
2023-08-24 07:35:10 -0700

    commit

---
+++ src/app/component/Group2/GroupTwo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const GroupTwo = () => {
+  return (
+    <div>
+        <div className='flex justify-start gap-64 text-white items-center bg-black h-[500px] mt-5'>
+            <p>ZIMO</p>
+            <p className='text-center'>We are the global platform. <br/> <span>Services in over one hundred and twenty countries.</span></p>
+
+        </div>
+    </div>
+  )
+}
+
+export default GroupTwo
